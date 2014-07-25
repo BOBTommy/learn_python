502d394714d09b54bf506123e67a82d5f564b9d7
riskkim <wnsgh611@gmail.com>
2014-07-26 01:16:10 +0900

    Example 11

---
+++ ex11.py
@@ -0,0 +1,13 @@
+__author__ = 'riskkim'
+
+#raw_input : read a sequence of input. it will end when it meets EOF.
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
