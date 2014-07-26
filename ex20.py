b5bd2bd316ffade04cf60ff2ddf128a09156491d
riskkim <wnsgh611@gmail.com>
2014-07-26 15:24:51 +0900

    Exercise 20

---
+++ ex20.py
@@ -0,0 +1,40 @@
+__author__ = 'riskkim'
+
+from sys import argv
+from os.path import exists
+
+script, input_file = argv
+
+def print_all(f):
+    print f.read()
+
+def rewind(f):
+    f.seek(0)
+
+def print_a_line(line_count, f):
+    print line_count, f.readline()
+
+if not exists(input_file):
+    print "The File is not exists."
+    exit(0)
+
+current_file = open(input_file)
+
+print "First let's print the whole file:\n"
+
+print_all(current_file)
+
+print "Now let's rewind, kind of like a tape."
+
+rewind(current_file) #Move file cursor to begin point
+
+print "Let's print three lines:"
+
+current_line = 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
+
+current_line += 1
+print_a_line(current_line, current_file)
